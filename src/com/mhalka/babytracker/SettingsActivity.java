df2048a21427e0f11d324deaa235c20789b015c3
almir <almir@dzinovic.net>
2012-03-19 01:03:17 +0100

    Dodana funkcija za otvaranje nove forme na Spasi dugme.

--- src/com/mhalka/babytracker/SettingsActivity.java
+++ src/com/mhalka/babytracker/SettingsActivity.java
@@ -2,9 +2,9 @@ package com.mhalka.babytracker;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -12,6 +12,8 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
+	
+	public static final String PREFS_NAME = "BabyTrackerPrefs";
 	
 	private RadioButton PracenjeTrudnoce;
 	private RadioButton PracenjeRazvoja;
@@ -24,6 +26,10 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
         
         PracenjeTrudnoce = (RadioButton) findViewById(R.id.rbPracenjeTrudnoce);
         PracenjeRazvoja = (RadioButton) findViewById(R.id.rbPracenjeRazvoja);
@@ -52,6 +58,17 @@ public class SettingsActivity extends Activity {
 					TerminPoroda.setVisibility(View.GONE);
 				    DatumRodjenja.setVisibility(View.VISIBLE);
 				}
+				
+			}
+		});
+        
+        Spasi.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(SettingsActivity.this, PregTracker.class);
+				startActivityForResult(intent, 0);
+				finish();
 				
 			}
 		});
