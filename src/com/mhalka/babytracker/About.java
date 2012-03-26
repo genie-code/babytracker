db389a40128807799be1f07210da9c6e4d4897ae
almir <almir@dzinovic.net>
2012-03-26 14:40:36 +0200

    Dodani meni i About forma.

---
+++ src/com/mhalka/babytracker/About.java
@@ -0,0 +1,32 @@
+package com.mhalka.babytracker;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class About extends Activity {
+	
+	private TextView VerzijaAplikacije;
+	private String VerzijaString;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.about);
+	    
+	    VerzijaAplikacije = (TextView) findViewById(R.id.txtVerzija);
+	    
+	    try {
+	    	PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+	    	VerzijaString = "Verzija "+ String.valueOf(pInfo.versionName);
+	    } catch (NameNotFoundException e) {
+	    		e.printStackTrace();
+	    		VerzijaString = "Ne mogu pronaæi verziju!";
+	    }
+	    
+	    VerzijaAplikacije.setText(VerzijaString);
+	}
+}
--- src/com/mhalka/babytracker/PregTracker.java
+++ src/com/mhalka/babytracker/PregTracker.java
@@ -3,29 +3,40 @@ package com.mhalka.babytracker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PregTracker extends Activity {
-	
-	private Button Postavke;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pregtracker);
-        
-        Postavke = (Button) findViewById(R.id.btnPostavke);
-        
-        Postavke.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent intent = new Intent(PregTracker.this, SettingsActivity.class);
- 				startActivityForResult(intent, 0);
- 				finish();
-			}
-		});
-        
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.podesavanja:
+            	Intent podesavanja = new Intent(this, SettingsActivity.class);
+ 				startActivityForResult(podesavanja, 0);
+                return true;
+            case R.id.about:
+            	Intent about = new Intent(this, About.class);
+ 				startActivityForResult(about, 0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
