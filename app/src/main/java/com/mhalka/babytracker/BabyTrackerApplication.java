b9b5b81f140d97f280bf2c17cfff0ac979d3105a
almir <almir@dzinovic.net>
2018-10-25 20:38:25 +0200

    - azuriran komentar

--- app/src/main/java/com/mhalka/babytracker/BabyTrackerApplication.java
+++ app/src/main/java/com/mhalka/babytracker/BabyTrackerApplication.java
@@ -11,8 +11,8 @@ public final class BabyTrackerApplication extends Application {
     }
 
     /*
-     * Fix for https://github.com/dmfs/opentasks/issues/383
-     * with workaround suggested at https://issuetracker.google.com/issues/36972466#comment14
+     * Workaround za framework bug na androidu 5.1: kada se app azurira dodje do padanja.
+     * Pronadjeno na: https://issuetracker.google.com/issues/36972466#comment14
      */
     private void checkAppReplacingState() {
         if (getResources() == null) {
